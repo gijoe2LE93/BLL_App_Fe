b70589c0188f6e0d5e11b03d7953445b985aec21
Anthony Simmmons <simmonsar@gmail.com>
2017-05-26 14:53:47 -0400

    added port requirements for heroku

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express     = require('express');
 var app         = express();
-var port        = 8000;
+var port        =  process.env.PORT || 8000;
+var bodyParser = require('body-parser');
 
 
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
 
 app.listen(port, function() {
     console.log("BLL app is listening in port " + port);
