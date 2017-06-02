b89677db6db48f5dc0fc891727cf3050fb9c4377
Anthony Simmmons <simmonsar@gmail.com>
2017-06-02 11:21:55 -0400

    changed address for heroku

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -2,7 +2,7 @@ console.log("BLL user.js");
 var app = angular.module('bllapp', []);
 
 app.controller('mainController', ['$http', function($http) {
-this.url = 'https://bll-app-fe.herokuapp.com' || 'http://localhost:3000';
+this.url = 'https://bll-app-be.herokuapp.com' || 'http://localhost:3000';
     // if(window.location.origin == "http://localhost:8000") {
     //   this.url = "http://localhost:3000";
     // }
@@ -93,7 +93,6 @@ this.url = 'https://bll-app-fe.herokuapp.com' || 'http://localhost:3000';
         console.log("getting users...");
         $http({
             method: 'GET',
-            // url: 'http://localhost:3000/users',
             url: this.url + '/users',
             }).then(function(response) {
                 console.log(response);
