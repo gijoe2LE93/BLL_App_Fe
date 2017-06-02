39fba28407ad34fe26eca7ce1a1441c5eaa8b037
Anthony Simmmons <simmonsar@gmail.com>
2017-06-02 10:32:53 -0400

    commented out local host for heroku test

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -2,7 +2,7 @@ console.log("BLL user.js");
 var app = angular.module('bllapp', []);
 
 app.controller('mainController', ['$http', function($http) {
-this.url = 'https://bll-app-fe.herokuapp.com/' || 'http://localhost:3000';
+this.url = 'https://bll-app-fe.herokuapp.com/'; //|| 'http://localhost:3000';
     // if(window.location.origin == "http://localhost:8000") {
     //   this.url = "http://localhost:3000";
     // }
